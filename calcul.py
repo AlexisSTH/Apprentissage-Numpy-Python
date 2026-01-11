c569ef61c12148ee5496751529f29bbbc66f9f35
Alexis <sayasith.alexis@gmail.com>
2026-01-11 16:46:21 +0100

    Importation d'exercice Numpy_base, & Ipython, Jupyter

---
+++ calcul.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+data = np.array([1, 2, 3, 4, 5])
+moyenne = np.mean(data)
+somme = np.sum(data)
+
+print("Data:", data)
+print("Moyenne: ", moyenne)
+print("Somme: ", somme)
---
+++ mon_script.py
@@ -0,0 +1,7 @@
+message = "Hello depuis mon script"
+x = 10
+y = 20
+resultat = x + y
+
+print(message)
+print(f"Résultat: {resultat}")
